f41588d405151cd8260dd38a7a78d63282b5f2ee
Sean <sanderson3619@gmail.com>
2018-10-02 19:24:41 -0700

    Menu
    Only the "Start Game" and "Gun X Name" and "Cheat" buttons work currently.

---
+++ Assets/GunSelection_Controller.cs
@@ -0,0 +1,57 @@
+﻿// Sean Anderson
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GunSelection_Controller : MonoBehaviour
+{
+    public int GunNumber;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // The user selects Gun #1
+    public void Gun1Selected()
+    {
+        // Store that they chose Gun #1
+        GunNumber = 1;
+        // Go to the main game
+        SceneManager.LoadScene("MainGame");
+    }
+
+    // The use selects Gun #2
+    public void Gun2Selected()
+    {
+        // Store that they chose Gun #2
+        GunNumber = 2;
+        // Go to the main game
+        SceneManager.LoadScene("MainGame");
+    }
+
+    // The user selects Gun #3
+    public void Gun3Selected()
+    {
+        // Store that they chose Gun #3
+        GunNumber = 3;
+        // Go to the main game
+        SceneManager.LoadScene("MainGame");
+    }
+
+    // The user selects Cheat
+    public void CheaterCheaterPumpkinEater()
+    {
+        // Store that the user wants to be a cheater and use Gun #4
+        GunNumber = 4;
+        // Go to the main game
+        SceneManager.LoadScene("MainGame");
+    }
+}
---
+++ Assets/Menu_Controller.cs
@@ -0,0 +1,45 @@
+﻿// Sean Anderson
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // The user clicks the Start Game button
+    public void StartGame ()
+    {
+        // Go to the main game
+        SceneManager.LoadScene("GunSelection");
+
+    }
+
+    // The user clicks the Quit Game button
+    public void QuitGame()
+    {
+        // Exit the unity.exe, does not work in the Unity Engine
+        Application.Quit();
+    }
+
+    // The user clicks the Help button
+    public void HelpScreen()
+    {
+        // Go to the help screen
+    }
+
+    // The user clicks on the Sound Settings button
+    public void SoundSettings()
+    {
+        // Go to the sound settings screen
+    }
+}
