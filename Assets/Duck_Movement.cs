9c701a37fe72e2cbb924f3ed215cfc1d6c468928
SamGouru <sam.gouru@gmail.com>
2018-10-04 04:50:27 -0700

    Sam's commit

--- Assets/Duck_Movement.cs
+++ Assets/Duck_Movement.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 public class Duck_Movement : GunSelection_Controller {
 
     public Vector2 position;
-    public int positionX = -1;
-    public int positionY = -1;
+    public float positionX;
+    public float positionY;
     public int score_Value = 0;
-    public int duck_speed = 10;
+    public int duck_speed = 7;
+    //SG
+    bool hit;
+
 
     // Use this for initialization
     void Start () {
                 
-        InvokeRepeating("duckMovement", 0.5f, 0.2f);
+        InvokeRepeating("DuckMovement", 0.5f, 0.2f);
 
     }
 
     // Movement of the duck is declared here
-    void duckMovement()
+    public void DuckMovement()
     {
         position = new Vector2(1 * positionX, positionY);
 
@@ -28,23 +31,25 @@ public class Duck_Movement : GunSelection_Controller {
         
         if (vectorX <= -7.5)
         {
-            positionX = 1;
+            positionX = 0.6F;
         }
         else if (vectorX >= 7.5)
         {
-            positionX = -1;
+            positionX = -0.6F;
         }
+        
         if (vectorY <= -3)
         {
-            positionY = 1;
+            positionY = 1F;
         }
         else if (vectorY >= 3)
         {
-            positionY = -1;
+            positionY = -1F;
         }
-
+        
         // translate, not position!! To add position to previous position
         GetComponent<Rigidbody2D>().transform.Translate(position);
+        
 
     }
     // Update is called once per frame
@@ -52,6 +57,7 @@ public class Duck_Movement : GunSelection_Controller {
 		
 	}
 
+    
     class Red_duck : Duck_Movement
     {
         private int negetive_value = -5;
@@ -63,4 +69,33 @@ public class Duck_Movement : GunSelection_Controller {
         private int positive_value = +5;
 
     }
+
+    //Changes made by Sam Gouru
+    public void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // stop all animations
+            CancelInvoke();
+            hit = true;
+
+            InvokeRepeating("DuckHit", 0.5f, 0.1f);
+        }
+    }
+
+
+    void DuckHit()
+    {
+        position = new Vector2(0, -1);
+        int ips = (int)GetComponent<Rigidbody2D>().transform.position.y;
+
+        GetComponent<Rigidbody2D>().transform.Translate(position);
+       
+        if (ips <= -3)
+        {
+            gameObject.SetActive(false);
+            CancelInvoke();
+        }
+    }
+
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,43 +4,58 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    public Texture2D cursorTexture;
-    public CursorMode cursorMode = CursorMode.Auto;
-    public Vector2 hotSpot = Vector2.zero;
+    //public Texture2D cursorTexture;
+    //public CursorMode cursorMode = CursorMode.Auto;
+    //public Vector2 hotSpot = Vector2.zero;
+  
 
     // Use this for initialization
     void Start () {
-        
-	}
+     
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+       /* Vector3 newPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Mathf.Abs(Camera.main.transform.position.z - transform.position.z)));
+        newPos.z = transform.position.z;
+        transform.position = newPos;
+        */
+    }
 
     //on awake
 
     private void Awake()
     {
-        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        //Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
     }
 
     // Handle mouse click
-    void onShoot()
+    public void onShoot()
     {
         if(Input.GetButtonDown("Fire1"))
         {
+            Debug.Log("Mouse click");
             sendLocation();
         }
     }
 
     // Send location co-ordinates
-    Vector2 sendLocation()
+    public Vector2 sendLocation()
     {
         Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
 
         System.Console.WriteLine(mousePosition);
+        Debug.Log(mousePosition);
 
         return mousePosition;
     }
+
+    /*
+    public void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("COLLIDE");
+        Destroy(collision.gameObject);
+        
+    }
+    */
 }
