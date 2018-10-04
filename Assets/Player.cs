976f317df200a5b4b1809dbe97b0ba5f19f5324e
abhilash-arcenal <abhilashreddy245@gmail.com>
2018-10-03 20:51:34 -0700

    player-initial-commit
    This commit should be enough for Sam to work.

---
+++ Assets/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //on awake
+
+    private void Awake()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+
+    // Handle mouse click
+    void onShoot()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            sendLocation();
+        }
+    }
+
+    // Send location co-ordinates
+    Vector2 sendLocation()
+    {
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+
+        System.Console.WriteLine(mousePosition);
+
+        return mousePosition;
+    }
+}
