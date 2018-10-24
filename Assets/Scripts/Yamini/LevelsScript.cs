7a4933768509e7051357c98102e208bd0ecc8689
Sean <sanderson3619@gmail.com>
2018-10-23 21:57:02 -0700

    Scene Selection

--- Assets/Scripts/Yamini/LevelsScript.cs
+++ Assets/Scripts/Yamini/LevelsScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Levels : MonoBehaviour
+public class LevelsScript : MonoBehaviour
 {
 
     public int scoreValue = 0;
