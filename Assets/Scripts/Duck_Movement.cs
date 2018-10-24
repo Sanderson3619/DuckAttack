e5d8d72dbfe8775ade72463437e5922d16e7e19a
Chiranjeevi97 <appalarouthu1997@gmail.com>
2018-10-23 21:23:37 -0700

    Enemy Spawning

--- Assets/Scripts/Duck_Movement.cs
+++ Assets/Scripts/Duck_Movement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Duck_Movement : GunSelection_Controller
+public class Duck_Movement : EnemyManager
 {
 
     public Vector2 position;
@@ -15,25 +15,24 @@ public class Duck_Movement : GunSelection_Controller
     // Use this for initialization
     void Start()
     {
-
-        InvokeRepeating("DuckMovement", 0.5f, 0.2f);
-
+        InvokeRepeating("DuckMovement", 0.5f, 0.3f);
+        
     }
 
     // Movement of the duck is declared here
     public void DuckMovement()
     {
-        position = new Vector2(1 * positionX, positionY);
+        position = new Vector2(positionX,  positionY);
 
         int vectorX = (int)GetComponent<Rigidbody2D>().transform.position.x;
         int vectorY = (int)GetComponent<Rigidbody2D>().transform.position.y;
 
 
-        if (vectorX <= -7.5)
+        if (vectorX <= -7.2)
         {
             positionX = 0.6F;
         }
-        else if (vectorX >= 7.5)
+        else if (vectorX >= 7.2)
         {
             positionX = -0.6F;
         }
@@ -49,27 +48,41 @@ public class Duck_Movement : GunSelection_Controller
 
         // translate, not position!! To add position to previous position
         GetComponent<Rigidbody2D>().transform.Translate(position);
-
-
     }
+
+//    public void Initialize()
+// 	{	
+//         UnityEngine.Debug.Log("noDucks");
+//         int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+		
+// 			if(maxDucks < 4)
+// 			{
+// 			Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+// 			noDucks++;	
+// 			maxDucks++;
+// 			UnityEngine.Debug.Log(noDucks);
+// 			} 			
+		
+// 	}
+
+    
     // Update is called once per frame
     void Update()
     {
 
     }
 
+    // class Red_duck : Duck_Movement
+    // {
+    //     private int negetive_value = -5;
 
-    class Red_duck : Duck_Movement
-    {
-        private int negetive_value = -5;
+    // }
 
-    }
+    // class Green_duck : Duck_Movement
+    // {
+    //     private int positive_value = +5;
 
-    class Green_duck : Duck_Movement
-    {
-        private int positive_value = +5;
-
-    }
+    // }
 
 }
 
---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+
+	public GameObject enemy;
+	public float spawnTime = 5f;
+	public Transform[] spawnPoints;
+	public Duck_Movement duck;
+	public static int noDucks = 0;
+	public static int maxDucks = 0;
+	
+	void Start()
+	{
+		duck = GetComponent<Duck_Movement>();
+		InvokeRepeating("Spawn", 0.1f, spawnTime);
+		        
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// if(duck.isDead == true)
+		// {
+		// 	Invoke("Spawn", 1);
+		// 	duck.isDead = false;
+		// }	
+		// UnityEngine.Debug.Log(noDucks);
+	}
+	
+	void Spawn()
+	{	
+		// duck.Initialize();
+		int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+		{
+			if(maxDucks < 4)
+			{
+			Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+			noDucks++;	
+			maxDucks++;
+			} 			
+		}
+		
+	}
+
+}
