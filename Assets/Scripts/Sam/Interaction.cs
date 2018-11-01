36ccf222d997581d4c4b9f1e7c464e96d2144c6c
Casey Kleinkopf <caseykleinkopf@gmail.com>
2018-10-31 22:29:29 -0700

    Fixed issue with interaction script

--- Assets/Scripts/Sam/Interaction.cs
+++ Assets/Scripts/Sam/Interaction.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class InteractionUFO : Duck_Movement
+public class Interaction : Duck_Movement
 {
     Text instruction;
     public static int score = 0;
