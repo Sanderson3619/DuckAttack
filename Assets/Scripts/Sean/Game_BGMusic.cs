6e78a14496fd09c6a5104969c05bc5b4eec7c4dd
Sean <sanderson3619@gmail.com>
2018-11-14 19:02:58 -0800

    Fixed menu sounds, maked things look prettier

---
+++ Assets/Scripts/Sean/Game_BGMusic.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_BGMusic : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    private static Game_BGMusic instance = null;
+    public static Game_BGMusic Instance {
+        get { return instance; }
+    }
+
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Update()
+    {
+        
+    }
+}
+// https://answers.unity.com/questions/11314/audio-or-music-to-continue-playing-between-scene-c.html
