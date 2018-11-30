74ef6bfce3e04d86fc225e68f46464ed2427390f
Sean <sanderson3619@gmail.com>
2018-11-29 20:10:24 -0800

    More Game Over Screen edits

--- Assets/Scripts/Sean/GameOverScreens.cs
+++ Assets/Scripts/Sean/GameOverScreens.cs
@@ -11,8 +11,8 @@ public class GameOverScreens : scoreupdatescript
     {
         GUIStyle fontStyle = new GUIStyle(GUI.skin.GetStyle("label"));
         fontStyle.fontSize = 32;
-        fontStyle.normal.textColor = Color.black;
+        fontStyle.normal.textColor = Color.white;
 
-        stringToEdit = GUI.TextField(new Rect(250, 350, 2000, 50), "You had a score of " + score.ToString() + "! You needed 20 to win.", fontStyle);
+        stringToEdit = GUI.TextField(new Rect(Screen.width/5, 0, 2000, 50), "<b>You had a score of </b>" + score.ToString() + "<b>! You needed 20 to win.</b>", fontStyle);
     }
 }
